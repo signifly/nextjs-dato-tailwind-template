201bd5661a3c39c26aa879edb93f71d17a748cf4
Duy Nguyen <duythedeveloper@gmail.com>
2024-02-15 21:03:53 -0500

    update datocms.json

--- graphql.config.js
+++ graphql.config.js
@@ -5,7 +5,7 @@ module.exports = {
     {
       'https://graphql.datocms.com': {
         headers: {
-          Authorization: `Bearer ${process.env.NEXT_DATOCMS_READ_ONLY_API_TOKEN}`,
+          Authorization: `Bearer ${process.env.DATOCMS_READ_ONLY_API_TOKEN}`,
           'X-Exclude-Invalid': true,
         },
       },
