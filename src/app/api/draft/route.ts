1a15ae4b8c484c884137b5ea63e98ceec7e253a4
Duy Nguyen <duythedeveloper@gmail.com>
2024-02-15 23:19:03 -0500

    update route to remove unnecessary env

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -32,7 +32,7 @@ export async function GET(request: NextRequest) {
   // Redirect to the homepage, or to the URL provided with the `redirect` query string parameter:
   const redirectUrl = new URL(
     searchParams.get('redirect') || '/',
-    process.env.NEXT_BASE_URL,
+    'https://example.com',
   )
 
   redirect(`${redirectUrl.pathname}${redirectUrl.search}`)
--- src/app/api/exit-draft/route.ts
+++ src/app/api/exit-draft/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
   draftMode().disable()
   const redirectUrl = new URL(
     searchParams.get('redirect') || '/',
-    process.env.NEXT_BASE_URL,
+    'https://example.com',
   )
 
   redirect(`${redirectUrl.pathname}${redirectUrl.search}`)
